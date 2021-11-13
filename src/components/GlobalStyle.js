5e30c73d01135c32632ac318d4a4a802e5fa31e2
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-13 10:53:09 -0500

    feat(app): add globalStyles component

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    background: #0f0e17;
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+
+  }
+  p{
+    margin:0;
+  }
+
+
+`;
