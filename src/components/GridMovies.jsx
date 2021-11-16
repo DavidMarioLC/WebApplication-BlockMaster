a515493d175b10f3029eb25d828acdc2032418ad
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-15 22:53:43 -0500

    refactor(gridMovies): use state movies and create filter by leastvalued, mostvalued and search

--- src/components/GridMovies.jsx
+++ src/components/GridMovies.jsx
@@ -1,14 +1,81 @@
-import styled from "styled-components";
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import CardMovie from '../components/CardMovie';
+import Spinner from '../components/Spinner';
+import TitleSection from '../components/TitleSection';
+import Modal from './Modal';
+import NotFoundSearch from './NotFoundSearch';
+const GridMovies = () => {
+  const { movies, filter, searchMoviesList, loading, modal } = useSelector(
+    (state) => state
+  );
 
-const GridMovies = ({ children }) => {
-  return <StyledGridMovies>{children}</StyledGridMovies>;
+  const AllMovies = () => {
+    return movies.map((movie) => <CardMovie key={movie.id} movie={movie} />);
+  };
+  const MostValuesMovies = () => {
+    const listMovies = movies.filter((movie) => movie.vote_average > 7);
+    return listMovies.map((movie) => (
+      <CardMovie key={movie.id} movie={movie} />
+    ));
+  };
+  const LestValuedMovies = () => {
+    const listMovies = movies.filter((movie) => movie.vote_average < 7);
+    return listMovies.map((movie) => (
+      <CardMovie key={movie.id} movie={movie} />
+    ));
+  };
+  const SearchMovies = () => {
+    return searchMoviesList.map((movie) => {
+      if (movie.poster_path !== null) {
+        return <CardMovie key={movie.id} movie={movie} />;
+      }
+      return null;
+    });
+  };
+
+  const getTitle = (filter) => {
+    //using a dictionary to get the title
+    const options = {
+      'least-valued': 'Menos valoradas',
+      'most-valued': 'Más valoradas',
+      search: 'Resultados de la busqueda',
+      all: 'Todas las Peliculas',
+    };
+    return options[filter];
+  };
+
+  return (
+    <>
+      <TitleSection>{getTitle(filter)}</TitleSection>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <StyledGridMovies>
+          {filter === 'all' ? (
+            <AllMovies />
+          ) : filter === 'most-valued' ? (
+            <MostValuesMovies />
+          ) : filter === 'least-valued' ? (
+            <LestValuedMovies />
+          ) : filter === 'search' ? (
+            <SearchMovies />
+          ) : null}
+        </StyledGridMovies>
+      )}
+      {filter === 'search' && searchMoviesList.length === 0 ? (
+        <NotFoundSearch />
+      ) : null}
+      {modal ? <Modal /> : null}
+    </>
+  );
 };
 
 const StyledGridMovies = styled.section`
   display: grid;
-  /* grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); */
   grid-template-columns: repeat(auto-fill, minmax(min-content, 220px));
   place-content: center;
+  align-items: center;
   gap: 1rem;
 `;
 
