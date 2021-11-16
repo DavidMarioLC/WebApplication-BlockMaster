17346cb6615f46eb6db1bf5b62fcf919634bb70a
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-15 22:50:07 -0500

    refactor(api): change the filename from api/videoApi to services/movie

--- src/api/videoApi.js
+++
@@ -1,8 +0,0 @@
-export const fetchVideos = async () => {
-  const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc`;
-
-  const response = await fetch(API_URL);
-  const data = await response.json();
-
-  return data;
-};
