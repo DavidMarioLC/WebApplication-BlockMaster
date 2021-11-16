eeff6cea73b98d1fbbd1180af07f38e0e62e00a6
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-15 23:00:11 -0500

    docs(app): add action types

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,7 @@
+export const INIT_MOVIES = 'INIT_MOVIES';
+export const SEARCH = 'SEARCH';
+export const LEST_VALUE_MOVIES = 'LEST_VALUE_MOVIES';
+export const MOST_VALUE_MOVIES = 'MOST_VALUE_MOVIES';
+export const SET_FILTER = 'SET_FILTER';
+export const LOADING = 'LOADING';
+export const SHOW_MODAL = 'SHOW_MODAL';
