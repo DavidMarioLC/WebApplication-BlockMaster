0eb00337c4a86cf077f38927a316617ae37de72b
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-15 22:56:16 -0500

    refactor(layoutmovies): add imports wrapper and styled

--- src/components/LayoutMovies.jsx
+++ src/components/LayoutMovies.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Wrapper } from "./Wrapper";
+import styled from 'styled-components';
+import { Wrapper } from './Wrapper';
 const LayoutMovies = ({ children }) => {
   return (
     <StyledLayoutMovies>
