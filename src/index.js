6290245c7228abfdf7a15d09d7f8d2170dce1fa0
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-15 22:48:29 -0500

    feat(app): add provider,store and react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import GridMovies from './components/GridMovies';
+import NotFound from './components/NotFound';
+import store from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<GridMovies />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
