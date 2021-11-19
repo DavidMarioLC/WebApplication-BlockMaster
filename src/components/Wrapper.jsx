a93876257b670e20792add3261e0f7059071886b
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-18 22:33:01 -0500

    feat(wrapper): add padding to vertical and horizontal

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   max-width: 1200px;
-  padding: 0 1rem;
+  padding: 1rem;
   margin: auto;
 `;
