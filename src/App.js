369087edaf8ca64b1ee33bd05f9917196c7df30e
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-18 22:38:46 -0500

    feat(app): config infinite scroll

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import { Outlet } from 'react-router-dom';
 import { GlobalStyle } from './components/GlobalStyle';
 import Header from './components/Header';
 import LayoutMovies from './components/LayoutMovies';
-import { initStateMovies } from './redux/actions';
+import Slider from './components/Slider';
+import { initGenderMovies, initStateMovies } from './redux/actions';
+
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(initGenderMovies());
     dispatch(initStateMovies());
+
+    const observer = new IntersectionObserver(([e]) => {
+      const { isIntersecting } = e;
+
+      if (isIntersecting) {
+        console.log(isIntersecting);
+        dispatch(initStateMovies());
+      }
+    });
+    observer.observe(window.intersector);
   }, [dispatch]);
 
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Slider />
       <LayoutMovies>
         <Outlet />
       </LayoutMovies>
+      <div id='intersector'></div>
     </>
   );
 };
