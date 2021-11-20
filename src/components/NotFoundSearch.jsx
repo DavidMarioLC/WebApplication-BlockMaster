7895396dccb4f0cc19bbfb7c009e03f82d2d6099
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-20 11:22:39 -0500

    refactor(search): delete styles button

--- src/components/NotFoundSearch.jsx
+++ src/components/NotFoundSearch.jsx
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import notFound from '../assets/images/notFound.svg';
 const NotFoundSearch = () => {
-  // const dispatch = useDispatch();
   const { searchWord } = useSelector((state) => state);
 
   return (
@@ -17,21 +16,10 @@ const NotFoundSearch = () => {
       <Title>
         No se encontrarón resultados para la busqueda de "{searchWord}"
       </Title>
-      {/* <ButtonBack onClick={() => dispatch(setFilterMovies('all'))}>
-        Regresar
-      </ButtonBack> */}
     </StyledNotFoundSearch>
   );
 };
 
-const ButtonBack = styled.button`
-  cursor: pointer;
-  background: none;
-  color: #fed941;
-  border: 1px solid #fed941;
-  padding: 0.7rem 2rem;
-  border-radius: 8px;
-`;
 const StyledNotFoundSearch = styled.div`
   display: flex;
   flex-direction: column;
