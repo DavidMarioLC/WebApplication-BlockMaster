8e0700e73c2b93555927621da2c978b665bd1f67
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-11-20 10:39:31 -0500

    doc(store): disabled redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { movieReducer } from './reducers';
 
 const store = createStore(
   movieReducer,
-
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
+  // composeWithDevTools()
 );
 
 export default store;
